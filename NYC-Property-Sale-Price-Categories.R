ed226529a06a17d84da382cab1fe8a2cf324e6f5
Will Powers <willpowers@MacBook-Pro.local>
2020-12-30 04:28:44 -0500

    adds Project Files

---
+++ NYC-Property-Sale-Price-Categories.R
@@ -0,0 +1,28 @@
+#	Rubric
+  #	Well commented
+  #	Code runs easily
+  #	Consistent w/ report
+  #	File Paths are relative
+  #	Missing packages installed automatically w/ if(!require) statements
+  # The dataset you use should either be automatically downloaded by your code or provided in your GitHub repo along with the rest of your files (Rmd, PDF, R).
+         # If your dataset is provided as a zip file in GitHub, your code should automatically unzip and load it.
+
+#Create Train & Validation sets with supplied code
+
+
+
+
+# Create Model w/ Train Set 
+
+     #  At least two different models or algorithms must be used, with at least one being more advanced than linear or logistic regression for prediction problems.
+
+
+
+
+#	Test Model with Validation Set
+
+
+
+
+#	Calculate RMSE
+
