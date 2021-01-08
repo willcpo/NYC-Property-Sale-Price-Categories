db4fc03eba5abbd430b026504f80a8e6f4b055ea
WP <willcpo@gmail..com>
2021-01-08 18:58:34 -0500

    updates files

--- fall-detection-analysis.R
+++ fall-detection-analysis.R
@@ -1,96 +1,201 @@
-#	Rubric
-  #	Well commented
-  #	Code runs easily
-  #	Consistent w/ report
-  #	File Paths are relative
-  #	Missing packages installed automatically w/ if(!require) statements
-  # The dataset you use should either be automatically downloaded by your code or provided in your GitHub repo along with the rest of your files (Rmd, PDF, R).
-         # If your dataset is provided as a zip file in GitHub, your code should automatically unzip and load it.
-
-##########################################################
-# Create Helper Functions if needed
-##########################################################
-
+###############################################################
 # Install Required Packages
+###############################################################
+set.seed(1, sample.kind="Rounding")
 options(install.packages.compile.from.source = "always")
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org", dependencies = TRUE)
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-if(!require(forecast)) install.packages('forecast', dependencies = TRUE, repos = "http://cran.us.r-project.org")
+if(!require(Rborist)) install.packages('Rborist', dependencies = TRUE, repos = "http://cran.us.r-project.org")
 
-###########################
+###############################################################
 # Import Data 
-###########################
+###############################################################
+fallData <- read_csv("https://raw.githubusercontent.com/willcpo/fall-detection-analysis/main/falldeteciton.csv")
 
-# TODO: FROM FILE
-liverData <- read_csv("https://raw.githubusercontent.com/willcpo/liver-disease-analysis/main/indian_liver_patient.csv")
-pca <- liverData %>% mutate(Gender = as.numeric(as.factor(Gender))) %>% prcomp()
-
-###########################
+###############################################################
 # Data Exploration
-###########################
+###############################################################
 
 # Check for NAN values
-sum(is.na(liverData))
-
-# remove NA values 
-liverData <- na.omit(liverData)
-
-# confirm NAs removed
-sum(is.na(liverData))
+amtNANValues <- sum(is.na(fallData))
+# None Detected
+amtNANValues
 
 # check for features with little variation
-nearZeroVar(liverData)
+columnsNearZero <- nearZeroVar(fallData)
+# None Detected
+columnsNearZero
 
+fallDataRowIndexes <- 1:length(fallData$ACTIVITY)
+rowCorrelation <- cor(fallDataRowIndexes, fallData$ACTIVITY)
+rowCorrelation
 
-
-###########################
-# Data Visualization
-###########################
-
-
-###########################
+###############################################################
 # Data Preparation
-###########################
+###############################################################
 
-#Make predictions factors for training model
-liverData <- liverData %>% mutate(Dataset=as.factor(Dataset))
+#Alter Activity Column as Falling or not falling
+fallData <- fallData %>% mutate(ACTIVITY=as.factor(ACTIVITY==3))
+#Reverses factor order to make TRUE the positive class
+fallData <- fallData %>% mutate(ACTIVITY=factor(ACTIVITY, levels=rev(levels(ACTIVITY)),labels = make.names(rev(levels(ACTIVITY)))))
 
-####################################
+
+###############################################################
 #Create Train & Validation sets
+###############################################################
+
+test_index <- createDataPartition(fallData$ACTIVITY, times = 1, p = 0.5, list = FALSE)
+
+validationSet <- fallData[test_index, ]
+remainderSet <- fallData[-test_index, ]
+
+test_index <- createDataPartition(remainderSet$ACTIVITY, times = 1, p = 0.25, list = FALSE)
+crossValidation1 <- remainderSet[test_index, ]
+remainderSet <- remainderSet[-test_index, ]
+
+test_index <- createDataPartition(remainderSet$ACTIVITY, times = 1, p = 0.33, list = FALSE)
+crossValidation2 <- remainderSet[test_index, ]
+remainderSet <- remainderSet[-test_index, ]
+
+test_index <- createDataPartition(remainderSet$ACTIVITY, times = 1, p = 0.5, list = FALSE)
+crossValidation3 <- remainderSet[test_index, ]
+crossValidation4 <- remainderSet[-test_index, ]
+
+########################################################################
+# Create KNN Model w/ 1st Cross Validation Set 
+########################################################################
+
+train_knn1 <- train(ACTIVITY ~ ., method = "knn", 
+                    data = crossValidation1,
+                    metric="Accuracy",
+                    tuneGrid = data.frame(k = seq(9, 71, 2)))
+# Plot best value of k
+plot_knn1 <-ggplot(train_knn1, highlight = TRUE)
+plot_knn1
+# Predict training set for confusion Matrix
+predictions_knn1 <- predict(train_knn1, crossValidation1)
+# Get Accuracy information with confusion matrix
+confusionMatrix_knn1 <- confusionMatrix(predictions_knn1, crossValidation1$ACTIVITY)
+confusionMatrix_knn1
+#
+# Redo optimizing for sensitivity
+#
+
+# Train using KNN
+train_knn2 <- train(ACTIVITY ~ ., method = "knn", 
+                    data = crossValidation1,
+                    tuneGrid = data.frame(k = seq(9, 71, 2)),
+                    metric = "Sens",
+                    trControl=trainControl(method="cv", 
+                                           number=5,
+                                           summaryFunction = twoClassSummary,
+                                           classProbs = TRUE,
+                                           savePredictions='all'))
+# Plot best value of k
+plot_knn2 <- ggplot(train_knn2, highlight = TRUE)
+plot_knn2
+# Predict training set for confusion Matrix
+predictions_knn2 <- predict(train_knn2, crossValidation1)
+# Get Accuracy information with confusion matrix
+confusionMatrix_knn2 <- confusionMatrix(predictions_knn2, crossValidation1$ACTIVITY)
+confusionMatrix_knn2
+########################################################################
+# Create QDA Model w/ 2nd Cross Validation Set 
+########################################################################
+
+train_qda <- train(ACTIVITY ~ ., method = "qda", 
+                   data = crossValidation2,
+                   metric = "Sens",
+                   trControl=trainControl(method="cv", 
+                                          number=5,
+                                          summaryFunction = twoClassSummary,
+                                          classProbs = TRUE,
+                                          savePredictions='all'))
+# Predict training set for confusion Matrix
+predictions_qda <- predict(train_qda, crossValidation2)
+# Get Accuracy information with confusion matrix
+confusionMatrix_qda <- confusionMatrix(predictions_qda, crossValidation2$ACTIVITY)
+confusionMatrix_qda
+
+########################################################################
+# Create Classification Tree Model w/ 3rd Cross Validation Set 
+########################################################################
+
+train_rpart <- train(ACTIVITY ~ ., method = "rpart", 
+                     data = crossValidation3,
+                     metric = "Sens",
+                     tuneGrid = data.frame(cp = seq(0.0, 0.1, len = 25)),
+                     trControl=trainControl(method="cv", 
+                                            number=5,
+                                            summaryFunction = twoClassSummary,
+                                            classProbs = TRUE,
+                                            savePredictions='all'))
+
+# Plot best value of k
+plot_rpart <- ggplot(train_rpart, highlight = TRUE)
+plot_rpart
+# Predict training set for confusion Matrix
+predictions_rpart <- predict(train_rpart, crossValidation3)
+# Get Accuracy information with confusion matrix
+confusionMatrix_rpart <- confusionMatrix(predictions_rpart, crossValidation3$ACTIVITY)
+confusionMatrix_rpart
+
+########################################################################
+# Create Random Forest Model w/ 4th Cross Validation Set 
+########################################################################
+
+train_rf <- train(ACTIVITY ~ ., method = "Rborist", 
+                  data = crossValidation4,
+                  metric = "Sens",
+                  tuneGrid = data.frame(predFixed = 2, minNode = c(3, 50)),
+                  trControl=trainControl(method="cv", 
+                                         number=5,
+                                         summaryFunction = twoClassSummary,
+                                         classProbs = TRUE,
+                                         savePredictions='all'))
+
+# Plot best value of k
+plot_rf <- ggplot(train_rf, highlight = TRUE)
+plot_rf
+
+# Predict training set for confusion Matrix
+predictions_rf <- predict(train_rf, crossValidation4)
+# Get Accuracy information with confusion matrix
+confusionMatrix_rf <- confusionMatrix(predictions_rf, crossValidation4$ACTIVITY)
+confusionMatrix_rf
+
+####################################
+#	Test Random Forest Model with Validation Set
 ####################################
 
-test_index <- createDataPartition(liverData$Age, times = 1, p = 0.5, list = FALSE)
+#Start Timer
+startTime <- proc.time()
 
-validationSet <- liverData[test_index, ]
-train_set <- liverData[-test_index, ]
+#Train
+train_validation <- train(ACTIVITY ~ ., method = "Rborist", 
+                          data = validationSet,
+                          metric = "Sens",
+                          tuneGrid = data.frame(predFixed = 2, minNode = 9),
+                          trControl=trainControl(method="cv", 
+                                                 number=5,
+                                                 summaryFunction = twoClassSummary,
+                                                 classProbs = TRUE,
+                                                 savePredictions='all'))
 
-####################################
-# Create Model w/ Train Set 
-####################################
-
-# TODO:  At least two different models or algorithms must be used, with at least one being more advanced than linear or logistic regression for prediction problems.
-
-
-model <- train(Dataset~., data=train_set, method="knn")
-
-confusionMatrix(model)
-# accuracy() ??
-####################################
-#	Test Model with Validation Set
-####################################
-
+#Stop Timer and Record time
+totalTime <- proc.time() - startTime
 ####################################
 # Accuracy and Performance
 ####################################
 
-# Confusion Matrix? 
-#confusionMatrix()
-# Accuracy ?
-# RMSE? or MSE?
-
-# Time Took by Final Modeling against Validation Set
-
+# Predict training set for confusion Matrix
+predictions_validation <- predict(train_validation, validationSet)
+# Get Accuracy information with confusion matrix
+confusionMatrix_validation <- confusionMatrix(predictions_validation, validationSet$ACTIVITY)
+confusionMatrix_validation
+# Time Taken in Seconds by Final Modeling against Validation Set
+totalTime["elapsed"]
 
 
 ####################################
@@ -98,5 +203,7 @@ confusionMatrix(model)
 ####################################
 
 # check PCA
-pca <- liverData %>% mutate(Gender = as.numeric(as.factor(Gender))) %>% prcomp(scale=TRUE, center=TRUE)
-summary(pca)
+pca <- fallData %>% mutate(ACTIVITY=as.numeric(ACTIVITY)) %>% prcomp(scale=TRUE, center=TRUE)
+pcaSumary <- summary(pca)
+pcaSumary
+
